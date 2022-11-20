ab509d00d33aaf49a5b0ab7c34fdcc12f189939a
Scott Clayton <scottclayton84@gmail.com>
2022-11-19 21:41:04 -0600

    Cleaned up the code

--- scripts/content.js
+++ scripts/content.js
@@ -2,12 +2,18 @@
 Scott Clayton 2022
 */
 
+var skotzDiscussionSelector = "[aria-label=Discussion]";
+var skotzEffortSelector = "[aria-label=Effort]";
+var skotzUsernameSelector = "#mectrl_currentAccount_secondary";
+var skotzEffortOptions = [1, 2, 3, 5, 8, 13, 20, 40, 100, "?"];
+
 function skotzHash(input) {
-    var hash = 0,
-        i, chr;
-    if (input.length === 0) return hash;
-    for (i = 0; i < input.length; i++) {
-        chr = input.charCodeAt(i);
+    var hash = 0;
+    if (input.length === 0) {
+        return hash;
+    }
+    for (var i = 0; i < input.length; i++) {
+        var chr = input.charCodeAt(i);
         hash = ((hash << 5) - hash) + chr;
         hash |= 0;
     }
@@ -16,20 +22,28 @@ function skotzHash(input) {
 
 function skotzCastVote(vote) {
     try {
-        document.querySelector("[aria-label=Discussion]").dispatchEvent(new Event("focus"));
-        document.querySelectorAll("[aria-label=Discussion] br").forEach(d => d.remove());
-        document.querySelectorAll("[aria-label=Discussion] .skotz-vote").forEach(d => d.remove());
+        // The first focus creates a <br> for some reason, so get that out of the way
+        document.querySelector(skotzDiscussionSelector).dispatchEvent(new Event("focus"));
+        document.querySelectorAll(skotzDiscussionSelector + " br").forEach(d => d.remove());
+
+        // Remove previous vote text (scores are left hidden)
+        document.querySelectorAll(skotzDiscussionSelector + " .skotz-vote").forEach(d => d.remove());
+
         // Hash email address for user id
-        var user = skotzHash(document.querySelector("#mectrl_currentAccount_secondary").innerHTML);
-        document.querySelector("[aria-label=Discussion]").innerHTML += "<span class=\"skotz-vote\">Voted</span><span style=\"font-size:0px\">{\"skotz\":true,\"vote\":\"" + vote + "\",\"user\":\"" + user + "\",\"time\":" + new Date().getTime() + "}</span>";
-        document.querySelector("[aria-label=Discussion]").dispatchEvent(new Event("focus"));
+        var user = skotzHash(document.querySelector(skotzUsernameSelector).innerHTML);
+
+        // Generate a hidden vote json (ADO apparently deleted hidden and custom tags on save, so a 0px font size is what I got working)
+        document.querySelector(skotzDiscussionSelector).innerHTML += "<span class=\"skotz-vote\">Voted</span><span style=\"font-size:0px\">{\"skotz\":true,\"vote\":\"" + vote + "\",\"user\":\"" + user + "\",\"time\":" + new Date().getTime() + "}</span>";
+        document.querySelector(skotzDiscussionSelector).dispatchEvent(new Event("focus"));
+
+        // ADO is using some control that uses a div as a text input, and it's hard to trigger the event so that a change is detected and the save button highlights, so repeatedly click the bold button until it takes
         var tries = 20;
         var timer = setInterval(function () {
             try {
-                if (document.querySelector("[aria-label=Discussion] .skotz-vote b") == null) {
+                if (document.querySelector(skotzDiscussionSelector + " .skotz-vote b") == null) {
                     const selection = window.getSelection();
                     const range = document.createRange();
-                    range.selectNodeContents(document.querySelector("[aria-label=Discussion] .skotz-vote"));
+                    range.selectNodeContents(document.querySelector(skotzDiscussionSelector + " .skotz-vote"));
                     selection.removeAllRanges();
                     selection.addRange(range);
                     document.querySelector(".discussion-control [aria-label=Bold]").dispatchEvent(new MouseEvent("click", { bubbles: true }));
@@ -50,12 +64,12 @@ function skotzCastVote(vote) {
 
 function skotzTallyVotes() {
     try {
+        // Find all the hidden json and parse out the scores
         var votes = [];
         document.querySelectorAll("span[style='font-size:0px']").forEach(x => {
             try {
                 var json = JSON.parse(x.innerHTML);
                 if (json.skotz === true) {
-                    //console.log(json);
                     var index = -1;
                     votes.forEach((v, i) => {
                         if (v.user == json.user) {
@@ -63,17 +77,19 @@ function skotzTallyVotes() {
                         }
                     });
                     if (index == -1) {
+                        // New user
                         votes.push(json);
-                        //console.log("new");
                     } else if (json.time > votes[index].time) {
+                        // Same user, newer vote based on timestamp
                         votes[index] = json;
-                        //console.log("newer");
                     }
                 }
             } catch (ex) {
                 console.log("bad " + x.innerHTML);
             }
         });
+
+        // Aggregate the votes gathered
         var scores = {};
         var unique = "!";
         votes.forEach(x => {
@@ -83,12 +99,15 @@ function skotzTallyVotes() {
             scores[x.vote]++;
             unique += "|" + x.vote + "|" + x.user + "|" + x.time;
         });
+
+        // Redraw when the state has changed
         var prev = document.querySelector(".skotz-graph");
-        // Redraw, if we need to
         if (prev == null || prev.getAttribute("data-last") != unique) {
             if (prev != null) {
                 prev.setAttribute("data-last", unique);
             }
+
+            // Draw a single line of the graph
             function skotzStyle(div, score) {
                 var scale = 16;
                 var width = 0;
@@ -111,36 +130,28 @@ function skotzTallyVotes() {
                 html += "</div>";
                 div.innerHTML += html;
             }
+
+            // Draw the graph directly below the effort box
             var div = document.createElement("div");
-            skotzStyle(div, 1);
-            skotzStyle(div, 2);
-            skotzStyle(div, 3);
-            skotzStyle(div, 5);
-            skotzStyle(div, 8);
-            skotzStyle(div, 13);
-            skotzStyle(div, 20);
-            skotzStyle(div, 40);
-            skotzStyle(div, 100);
-            skotzStyle(div, "?");
+            skotzEffortOptions.forEach(z => skotzStyle(div, z));
             div.classList.add("skotz-graph");
             div.setAttribute("data-last", unique);
-            var parent = document.querySelector("[aria-label=Effort]");
+            var parent = document.querySelector(skotzEffortSelector);
             while (parent != null && !parent.classList.contains("control")) {
+                // Find the nearest "control" parent
                 parent = parent.parentElement;
             }
             if (parent != null) {
                 document.querySelectorAll(".skotz-graph").forEach(d => d.remove());
                 parent.appendChild(div);
-                document.getElementById("skotz-line-1").addEventListener("click", function (e) { skotzCastVote(1); skotzTallyVotes(); }, false);
-                document.getElementById("skotz-line-2").addEventListener("click", function (e) { skotzCastVote(2); skotzTallyVotes(); }, false);
-                document.getElementById("skotz-line-3").addEventListener("click", function (e) { skotzCastVote(3); skotzTallyVotes(); }, false);
-                document.getElementById("skotz-line-5").addEventListener("click", function (e) { skotzCastVote(5); skotzTallyVotes(); }, false);
-                document.getElementById("skotz-line-8").addEventListener("click", function (e) { skotzCastVote(8); skotzTallyVotes(); }, false);
-                document.getElementById("skotz-line-13").addEventListener("click", function (e) { skotzCastVote(13); skotzTallyVotes(); }, false);
-                document.getElementById("skotz-line-20").addEventListener("click", function (e) { skotzCastVote(20); skotzTallyVotes(); }, false);
-                document.getElementById("skotz-line-40").addEventListener("click", function (e) { skotzCastVote(40); skotzTallyVotes(); }, false);
-                document.getElementById("skotz-line-100").addEventListener("click", function (e) { skotzCastVote(100); skotzTallyVotes(); }, false);
-                document.getElementById("skotz-line-x").addEventListener("click", function (e) { skotzCastVote("?"); skotzTallyVotes(); }, false);
+
+                // Attach click events for updating votes
+                skotzEffortOptions.forEach(z => {
+                    document.getElementById("skotz-line-" + (z == "?" ? "x" : z)).addEventListener("click", function (e) {
+                        skotzCastVote(z);
+                        skotzTallyVotes();
+                    }, false);
+                });
             }
         }
     } catch (ex) {
@@ -150,4 +161,4 @@ function skotzTallyVotes() {
 
 setInterval(skotzTallyVotes, 1000);
 
-console.log("%c Loaded Skotz Plugin ", "background:#0F0;color:#000;font-size:20px;");
+console.log("%c Loaded Skotz Plugin v1.0 ", "background:#0F0;color:#000;font-size:20px;");
